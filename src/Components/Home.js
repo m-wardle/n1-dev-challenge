6270afc352140c2b9adae700ac66d53e97901d31
Matt Wardle <mwardle.22@gmail.com>
2020-01-22 18:45:07 -0500

    Add buttons to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,76 +4,93 @@ import Grid from '@material-ui/core/Grid';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import FiberNewIcon from '@material-ui/icons/FiberNew';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
 const Home = () => {
-  return (
-    <Grid
-      container
-      direction="row"
-      justify="space-around"
-      alignItems="center"
-      style={styles.grid}
-    >
-      <ButtonBase
-        focusRipple
-        type="button"
-        onClick={() => console.log("hello")}
+  const [newTask, setNewTask] = useState(false);
+  const [viewTasks, setViewTasks] = useState(false);
+
+
+  if (newTask) {
+    return <Redirect to="/tasks/new" />;
+  } else if (viewTasks) {
+    return <Redirect to="/tasks" />
+  } else {
+    return (
+      <Grid
+        container
+        direction="row"
+        justify="space-around"
+        alignItems="center"
+        style={styles.grid}
       >
-        <Paper
-          elevation={3}
-          style={styles.paper}
+        <ButtonBase
+          focusRipple
+          type="button"
+          onClick={() => setNewTask(true)}
         >
-          <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-            style={styles.subGrid}
+          <Paper
+            elevation={3}
+            style={styles.paper}
           >
-            {/* <PostAddIcon style={styles.icon} /> */}
-            <Typography
-              variant='h5'
-              align="center"
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              style={styles.subGrid}
             >
-              New Task
+              <FiberNewIcon style={styles.icon} />
+              <Typography
+                variant='h5'
+                align="center"
+              >
+                New Task
             </Typography>
-          </Grid>
-        </Paper>
-      </ButtonBase>
-      <ButtonBase
-        focusRipple
-        type="button"
-        onClick={() => console.log("hello")}
-      >
-        <Paper
-          elevation={3}
-          style={styles.paper}
+            </Grid>
+          </Paper>
+        </ButtonBase>
+        <ButtonBase
+          focusRipple
+          type="button"
+          onClick={() => setViewTasks(true)}
         >
-          <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-            style={styles.subGrid}
+          <Paper
+            elevation={3}
+            style={styles.paper}
           >
-            {/* <PostAddIcon style={styles.icon} /> */}
-            <Typography
-              variant='h5'
-              align="center"
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              style={styles.subGrid}
             >
-              View Tasks
+              <AssignmentIcon style={styles.icon} />
+              <Typography
+                variant='h5'
+                align="center"
+              >
+                View Tasks
             </Typography>
-          </Grid>
-        </Paper>
-      </ButtonBase>
-    </Grid>
-  );
+            </Grid>
+          </Paper>
+        </ButtonBase>
+      </Grid>
+    )
+  }
 }
 
 const styles = {
   grid: {
     marginTop: "1em",
     height: "80vh"
+  },
+  icon: {
+    fontSize: "20em",
+    color: "#3f51b5",
+    padding: ".2em"
   },
   subGrid: {
     height: "60vh",
