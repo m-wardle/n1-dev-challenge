f060b8444a1cc13f563a3e14eb1c19dceacf746f
Matt Wardle <mwardle.22@gmail.com>
2020-01-22 20:06:10 -0500

    Add minor styling/organization

--- src/Components/TaskItem.js
+++ src/Components/TaskItem.js
@@ -3,6 +3,10 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography'
+import green from '@material-ui/core/colors/green';
+import yellow from '@material-ui/core/colors/yellow';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
 import { Redirect } from 'react-router';
 
 const TaskItem = ({ task, tasks, updateTasks }) => {
@@ -57,63 +61,74 @@ const TaskItem = ({ task, tasks, updateTasks }) => {
       (
         !task.isDeleted ?
           <Paper key={task.id} style={styles.paper}>
-            <Grid item key={task.id}>
+            <Grid
+              container
+              direction="column"
+              alignItems="center"
+            >
               <Typography variant="h4">
                 {task.title}
               </Typography>
               <section>
                 <p>Description: {task.description}</p>
-                <p>Due Date: {task.dueDate} hours</p>
+                <p>Due Date: {task.dueDate}</p>
                 <p>Status: {taskStatus(task)}</p>
-                {
-                  taskStatus(task) === "Pending" ?
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      style={styles.button}
-                      onClick={() => {
-                        startTask(task.id)
-                      }
-                      }>
-                      Start Task
-                  </Button> : null
-                }
-                {
-                  taskStatus(task) === "In Progress" ?
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      style={styles.button}
-                      onClick={() => {
-                        completeTask(task.id)
-                      }
-                      }>
-                      Complete Task
-                  </Button> : null
-                }
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  style={styles.button}
-                  onClick={() => {
-                    setEdit(true)
+                <Grid
+                  container
+                  direction="row"
+                  justify="center"
+                >
+                  {
+                    taskStatus(task) === "Pending" ?
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        style={styles.accept}
+                        onClick={() => {
+                          startTask(task.id)
+                        }
+                        }>
+                        Start Task
+                    </Button> : null
                   }
-                  }>
-                  Edit Task
-                  </Button>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  style={styles.button}
-                  onClick={() => {
-                    deleteTask(task.id)
+                  {
+                    taskStatus(task) === "In Progress" ?
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        style={styles.complete}
+                        onClick={() => {
+                          completeTask(task.id)
+                        }
+                        }>
+                        Complete Task
+                    </Button> : null
                   }
-                  }>
-                  Delete Task
-                  </Button>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    style={styles.edit}
+                    onClick={() => {
+                      setEdit(true)
+                    }
+                    }>
+                    Edit Task
+                </Button>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    style={styles.delete}
+                    onClick={() => {
+                      deleteTask(task.id)
+                    }
+                    }>
+                    Delete Task
+                </Button>
+                </Grid>
+
               </section>
             </Grid>
-          </Paper> : null
+          </Paper > : null
       )
   );
 }
@@ -126,9 +141,29 @@ const styles = {
     marginRight: 50,
     padding: "2em"
   },
-  button: {
-    width: "100%",
-    padding: "auto"
+  accept: {
+    width: "8em",
+    margin: "1em",
+    color: "black",
+    backgroundColor: green[500]
+  },
+  edit: {
+    width: "8em",
+    margin: "1em",
+    color: "black",
+    backgroundColor: yellow[500]
+  },
+  complete: {
+    width: "8em",
+    margin: "1em",
+    color: "black",
+    backgroundColor: blue[500]
+  },
+  delete: {
+    width: "8em",
+    margin: "1em",
+    color: "black",
+    backgroundColor: red[500]
   },
   container: {
     width: "100%",
--- src/Components/ViewTasks.js
+++ src/Components/ViewTasks.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import TaskItem from './TaskItem';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const ViewTasks = ({ tasks, updateTasks }) => {
 
   const taskList = tasks.map((task) => {
     return (
       <TaskItem
+        key={task.id}
         task={task}
         tasks={tasks}
         updateTasks={updateTasks}
@@ -18,7 +20,13 @@ const ViewTasks = ({ tasks, updateTasks }) => {
       <Typography variant="h2" align="center">
         To-Do
       </Typography>
-      {taskList}
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+      >
+        {taskList}
+      </Grid>
     </React.Fragment>
   );
 }
